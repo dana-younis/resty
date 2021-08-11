800cb680985d139ef674e32f697c634782db9952
dana-younis <younisdana7@gmail.com>
2021-08-11 17:52:53 +0300

    27

--- src/components/form/form.test.js
+++ src/components/form/form.test.js
@@ -11,48 +11,63 @@ it('need to run a function on button click', async () => {
   fireEvent.click(button);
   await waitFor(() => expect(callApi).toHaveBeenCalled());
 });
-
 it('Should render results', () => {
-  const result = {
-    "Headers": {
-      "content-type": "string application/json"
-    },
-    "count": 2,
-    "results": [
-      {
-        "name": "fake thing 1",
-        "url": "http://fakethings.com/1"
+  const data = {
+      Headers:{
+      'content-type': 'string application/json',
       },
-      {
-        "name": "fake thing 2",
-        "url": "http://fakethings.com/2"
-      }
-    ]
+      count: 2,
+      results: [
+          {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+          {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+      ],
   };
-
-  render(<Results data={result} />);
-
+  render(<Results data={data} />);
   const items = screen.getByTestId('renderedData');
-
-  expect(items).toHaveTextContent('fake thing 1');
-  expect(items).toContainHTML(
-    `<pre data-testid="renderedData">{
-  "Headers": {
-    "content-type": "string application/json"
-  },
-  "count": 2,
-  "results": [
-    {
-      "name": "fake thing 1",
-      "url": "http://fakethings.com/1"
-    },
-    {
-      "name": "fake thing 2",
-      "url": "http://fakethings.com/2"
-    }
-  ]
-}</pre>`
-  );
-  expect(items).toHaveTextContent('http://fakethings.com/2');
-  expect(items).toHaveTextContent('Headers');
+  expect(items).toHaveTextContent('{ "Headers": { "content-type": "string application/json" }, "count": 2, "results": [ { "name": "fake thing 1", "url": "http://fakethings.com/1" }, { "name": "fake thing 2", "url": "http://fakethings.com/2" } ] }');
 });
+
+// it('Should render results', () => {
+//   const result = {
+//     "Headers": {
+//       "content-type": "string application/json"
+//     },
+//     "count": 2,
+//     "results": [
+//       {
+//         "name": "fake thing 1",
+//         "url": "http://fakethings.com/1"
+//       },
+//       {
+//         "name": "fake thing 2",
+//         "url": "http://fakethings.com/2"
+//       }
+//     ]
+//   };
+
+//   render(<Results data={result} />);
+
+//   const items = screen.getByTestId('renderedData');
+
+//   expect(items).toHaveTextContent('fake thing 1');
+//   expect(items).toContainHTML(
+//     `<pre data-testid="renderedData">{
+//   "Headers": {
+//     "content-type": "string application/json"
+//   },
+//   "count": 2,
+//   "results": [
+//     {
+//       "name": "fake thing 1",
+//       "url": "http://fakethings.com/1"
+//     },
+//     {
+//       "name": "fake thing 2",
+//       "url": "http://fakethings.com/2"
+//     }
+//   ]
+// }</pre>`
+//   );
+//   expect(items).toHaveTextContent('http://fakethings.com/2');
+//   expect(items).toHaveTextContent('Headers');
+// });
